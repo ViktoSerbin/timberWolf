83203cc43493f6cfa1b40f900a0bf7902b5c24b2
ViktoSerbin <taur1721@gmail.com>
2022-07-23 18:04:14 +0300

    aded galleryslider

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,11 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+
+document.body.addEventListener("click", function (e) {
+    const targetElement = e.target;
+    if (targetElement.closest('.featured__item-like')) {
+        targetElement.classList.toggle('_active');
+    }
+});
--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -104,6 +104,84 @@ function initSliders() {
 			}
 		});
 	}
+
+	if (document.querySelector('.galleryslider__slider')) { // Указываем скласс нужного слайдера
+		// Создаем слайдер
+		new Swiper('.galleryslider__slider', { // Указываем скласс нужного слайдера
+			// Подключаем модули слайдера
+			// для конкретного случая
+			modules: [Navigation, EffectFade],
+			observer: true,
+			observeParents: true,
+			slidesPerView: 1,
+			spaceBetween: 0,
+			autoHeight: true,
+			speed: 1000,
+			effect: 'fade',
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			/*
+			// Эффекты
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Пагинация
+			/*
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			*/
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "влево/вправо"
+			navigation: {
+				prevEl: '.galleryslider__head-navigation .swiper-button-prev',
+				nextEl: '.galleryslider__head-navigation .swiper-button-next',
+			},
+
+			// Брейкпоинты
+			/*
+			breakpoints: {
+				320: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+			// События
+			on: {
+
+			}
+		});
+	}
 }
 // Скролл на базе слайдера (по классу swiper_scroll для оболочки слайдера)
 function initSlidersScroll() {
